98626e8e0032f85e983766cd62c758dd9af52040
astra <26215963+3tternp@users.noreply.github.com>
2025-06-08 21:37:41 +0545

    Update generate_env.sh

--- generate_env.sh
+++ generate_env.sh
@@ -6,9 +6,9 @@ cat <<EOF > .env
 DEBUG=True
 SECRET_KEY=changeme123
 ALLOWED_HOSTS=localhost,127.0.0.1
-DB_NAME=rmfdb
-DB_USER=rmfuser
-DB_PASSWORD=rmfpass
+DB_NAME=postgres
+DB_USER=postgres
+DB_PASSWORD=postgres
 DB_HOST=db
 DB_PORT=5432
 REACT_APP_API_URL=http://localhost:8000
