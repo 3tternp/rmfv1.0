dc8e4b5a9f4f326bf2bb71fdcdec27e65ea2cacd
astra <26215963+3tternp@users.noreply.github.com>
2025-06-08 20:49:01 +0545

    Update api_urls.py

--- backend/rmf_api/api_urls.py
+++ backend/rmf_api/api_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import RiskListCreateView, RiskDetailView
+from .views import CreateUserView
 
-urlpatterns = [
+urlpatterns += [
     path('risks/', RiskListCreateView.as_view(), name='risk-list'),
     path('risks/<int:pk>/', RiskDetailView.as_view(), name='risk-detail'),
+    path('create-user/', CreateUserView.as_view(), name='create-user'),
 ]
