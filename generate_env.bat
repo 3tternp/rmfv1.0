e989440e409cfe7bb9c1874f4892166b484b926b
astra <26215963+3tternp@users.noreply.github.com>
2025-06-08 16:14:24 +0545

    Create generate_env.bat

---
+++ generate_env.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Creating .env file...
+
+(
+echo DEBUG=True
+echo SECRET_KEY=changeme123
+echo ALLOWED_HOSTS=localhost,127.0.0.1
+echo DB_NAME=rmfdb
+echo DB_USER=rmfuser
+echo DB_PASSWORD=rmfpass
+echo DB_HOST=db
+echo DB_PORT=5432
+echo REACT_APP_API_URL=http://localhost:8000
+) > .env
+
+echo .env file created successfully.
+pause
