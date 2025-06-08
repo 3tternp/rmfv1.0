1830c06ffb34d5f2f813f617993e0783ee018a2d
astra <26215963+3tternp@users.noreply.github.com>
2025-06-08 14:49:47 +0545

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import RiskRegister from './components/RiskRegister';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/risks" element={<RiskRegister />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
